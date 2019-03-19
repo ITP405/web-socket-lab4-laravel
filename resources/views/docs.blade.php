ad8d5fa9f36ddb3bed4f82652d2d89b6723f1ee4
jaejunmin <jaejunmi@usc.edu>
2019-03-19 16:54:17 -0700

    laravel

---
+++ resources/views/docs.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div id="editor" contenteditable="true">
+    This text can be edited by the user.
+    </div>
+    <script src="Users/jaejunmin/Desktop/itp405/lab4_Web_Socket/main.js">
+    </script>
+  </body>
+</html>
