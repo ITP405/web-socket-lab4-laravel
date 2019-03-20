2fb7ad947df25caa79e09f174e5123656174324e
jaejunmin <jaejunmi@usc.edu>
2019-03-19 22:43:44 -0700

    wss fixed

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -8,9 +8,9 @@
     <div id="editor" contenteditable="true">
     This text can be edited by the user.
     </div>
-    <script src="https://raw.githubusercontent.com/ITP405/lab4-web-socket-node/master/main.js"></script>
+    <!-- <script src="https://raw.githubusercontent.com/ITP405/lab4-web-socket-node/master/main.js"></script> -->
       <!-- <script src="/main.js"></script> -->
-      <!-- <script src="{{'node/main.js'}}"></script> -->
+      <script src="{{'node/main.js'}}"></script>
       <!-- <script src="ws://web-socket-node-jaejun.herokuapp.com"></script> -->
       <!-- <script src="wss://web-socket-node-jaejun.herokuapp.com/main.js"></script> -->
 
