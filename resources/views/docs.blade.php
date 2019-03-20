68aff68a5d788335b8eb5c6b04ec5110ce847fdd
jaejunmin <jaejunmi@usc.edu>
2019-03-19 18:45:01 -0700

    m

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -8,6 +8,7 @@
     <div id="editor" contenteditable="true">
     This text can be edited by the user.
     </div>
-    <script src="https://cdn.jsdelivr.net/gh/ITP405/lab4-web-socket-node/main.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/gh/ITP405/lab4-web-socket-node/main.js"></script> -->
+      <script src="../js/node/main.js"></script>
   </body>
 </html>
